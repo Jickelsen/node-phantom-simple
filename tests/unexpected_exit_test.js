53fbecebb1583bb651a2e679a5b9364e50dff855
Evgeny Poberezkin <e.poberezkin@me.com>
2015-05-09 17:07:32 +0100

    unexpected exit test

---
+++ tests/unexpected_exit_test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var phantom = require('../node-phantom-simple')
+    , assert = require('assert');
+
+describe('phantom', function() {
+    it('should not open page after phantom exit', function() {
+        return phantom.create()
+        .bind({})
+        .then(function (ph) {
+            this.ph = ph;
+            return ph.createPage();
+        })
+        .then(function (page) {
+            this.ph.exit()
+            return page.open('http://www.google.com');
+        })
+        .then(function (status) {
+            assert.notEqual(status, 'success');
+            throw new Error('should not open page');
+        })
+        .catch(function (err) {
+            assert(!!err);
+        });
+    });
+});
