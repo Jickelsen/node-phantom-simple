028d0e5d2399413d01142608129715848428f271
Evgeny Poberezkin <e.poberezkin@me.com>
2015-05-12 01:17:48 +0100

    test for page.waitForSelector

---
+++ tests/page_extras_test.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var phantom = require('../node-phantom-async')
+    , createServer = require('./utils/create_server')
+    , assert = require('assert');
+
+
+describe('page extras', function() {
+    var server, ph, page, messages;
+
+
+    before(function() {
+        return createServer('<html><head></head>\
+<body><h1>Hello World</h1><button id="button1" class="button1">Button 1</button></body></html>')
+        .then(function(_server) {
+            server = _server;
+            return phantom.create({ignoreErrorPattern: /CoreText performance note/});
+        })
+        .then(function (_ph) {
+            ph = _ph;
+        });
+    });
+
+
+    beforeEach(function() {
+        messages = [];
+        return ph.createPage()
+        .then(function (_page) {
+            page = _page;
+            return page.open('http://localhost:' + server.address().port);
+        })
+        .then(function (status) {
+            assert.equal(status, 'success');
+            // page.onConsoleMessage = function (msg) { messages.push(msg); };
+            return page.evaluate(browserPrepareTestPage);
+        });
+    });
+
+    function browserPrepareTestPage() {
+        var button1 = document.getElementById('button1');
+        listen(button1);
+
+        setTimeout(function() {
+            var button2 = document.createElement('button');
+            button2.id = 'button2';
+            button2.innerHTML = 'Button 2';
+            document.body.appendChild(button2);
+            listen(button2);
+        }, 500);
+
+        function listen(button) {
+            button.addEventListener('click', function(e) {
+                console.log(button.id + ' clicked');
+            });
+        }
+    }
+
+    afterEach(function() {
+        return page.close();
+    });
+
+    after(function (done) {
+        ph.exit()
+        .finally(function() {
+            server.close(done);
+        });
+    });
+
+
+    describe.only('waitForSelector method', function() {
+        it('should resolve if selector exists', function() {
+            return page.waitForSelector('#button1');
+        });
+
+        it('should resolve if selector appears', function() {
+            return page.waitForSelector('#button2');
+        });
+
+        it('should reject if timeout expires before selector appears', function() {
+            return isRejected(page.waitForSelector('#button2', 400));
+        });
+
+        it('should reject if selector does not exist', function() {
+            return isRejected(page.waitForSelector('#button3', 1000));
+        });
+    });
+
+
+    function isRejected(promise) {
+        return promise
+        .then(function() {
+            throw new Error('promise is resolved')
+        })
+        .catch(function(err) {
+            assert(!!err);
+        });
+    }
+});
